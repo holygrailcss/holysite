2509b9297686a6bdb50db2c48fa3a7251e263b2b
Marc Fernandez <marcfl@ext.massimodutti.com>
2020-04-07 16:55:43 +0200

    Add JSHint linter and rewrite gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
-const gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssnano = require('cssnano'),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
 // Task for compile styles
 function style()
 {
     return (
         gulp
-           // .src('./scss/tags.scss')
             .src('./scss/style.scss')
             .pipe(sourcemaps.init())
             .pipe(sass())
             .on('error', sass.logError) 
             .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(sourcemaps.write())
-          //  .pipe(rename('styles.min.css'))
+            .pipe(rename('styles.min.css'))
             .pipe(gulp.dest('./dist'))
     );
 }
 
-function watch() {
+function watch()
+{
     browserSync.init({
         server: {
             baseDir: './'
         }
-    })
-    gulp.watch('./scss/**/*.scss', style)
+    });
+    
+    gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./**/*.html').on('change', browserSync.reload);
 }
 
