bb98f806a5523818f11a519a5c4008cedf25a898
manuelruizredondo <manuelruizredondo@gmail.com>
2020-04-07 16:12:20 +0200

    nuevos elementos

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,39 @@ const gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssnano = require('cssnano'),
-    rename = require('gulp-rename')
-    sourcemaps = require('gulp-sourcemaps');
+    rename = require('gulp-rename'),
+    sourcemaps = require('gulp-sourcemaps'),
+    browserSync = require('browser-sync').create();
 
 // Task for compile styles
 function style()
 {
     return (
         gulp
+           // .src('./scss/tags.scss')
             .src('./scss/style.scss')
             .pipe(sourcemaps.init())
             .pipe(sass())
             .on('error', sass.logError) 
             .pipe(postcss([autoprefixer(), cssnano()]))
             .pipe(sourcemaps.write())
-            .pipe(rename('styles.min.css'))
+          //  .pipe(rename('styles.min.css'))
             .pipe(gulp.dest('./dist'))
     );
 }
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./scss/**/*.scss', style)
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+}
+
+
  
 // Expose the task by exporting it
 exports.style = style;
+exports.watch=watch;
